261c89c4001e5c3f39620be27d5619666dcd1d9b
wintyo <wintyo1024@gmail.com>
2020-04-24 11:39:44 +0900

    refの設定のやり方が分かったので試す

--- pages/index.vue
+++ pages/index.vue
@@ -2,35 +2,46 @@
 div
   .block
     StandardPug(
+      ref="comStandardPug"
       :value="10"
     )
   .block
     StandardHtml(
+      ref="comStandardHtml"
       :value="100"
     )
   .block
     TypedVueHtml(
+      com="comTypedVueHtml"
       :value="0"
     )
   .block
     ClassHtml(
+      ref="comClassHtml"
       :value="5"
     )
   .block
     ClassHtmlInjectParams(
+      ref="comClassHtmlInjectParams"
       :value="5"
     )
   .block
     ClassTypedVueHtml(
+      ref="comClassTypedVueHtml"
       :value="5"
     )
   .block
     CompositionHtml(
+      ref="comCompositionHtml"
       :value="0"
     )
 </template>
 
 <script lang="ts">
+import Vue from 'vue';
+import { Vue as IVue, VueConstructor as IVueConstructor } from 'vue/types/vue';
+
+// components
 import StandardPug from '@/components/StandardPug.vue';
 import StandardHtml from '@/components/StandardHtml.vue';
 import TypedVueHtml from '@/components/TypedVueHtml.vue';
@@ -39,7 +50,22 @@ import ClassHtmlInjectParams from '@/components/ClassHtmlInjectParams.vue';
 import ClassTypedVueHtml from '@/components/ClassTypedVueHtml.vue';
 import CompositionHtml from '@/components/CompositionHtml.vue';
 
-export default {
+interface IExtendedVue extends IVue {
+  $refs: {
+    comStandardPug: InstanceType<typeof StandardPug>;
+    comStandardHtml: InstanceType<typeof StandardHtml>;
+    comTypedVueHtml: InstanceType<typeof TypedVueHtml>;
+    // クラスはそのまま指定できる
+    comClassHtml: ClassHtml;
+    // InstanceTypeの書き方でもできる
+    // comClassHtml: InstanceType<typeof ClassHtml>;
+    comClassHtmlInjectParams: ClassHtmlInjectParams;
+    comClassTypedVueHtml: ClassTypedVueHtml;
+    comCompositionHtml: InstanceType<typeof CompositionHtml>;
+  }
+}
+
+export default (Vue as IVueConstructor<IExtendedVue>).extend({
   components: {
     StandardPug,
     StandardHtml,
@@ -49,7 +75,12 @@ export default {
     ClassTypedVueHtml,
     CompositionHtml,
   },
-}
+  mounted() {
+    this.$refs.comStandardPug;
+    this.$refs.comClassHtml;
+    this.$refs.comClassHtmlInjectParams.$props;
+  },
+});
 </script>
 
 <style lang="scss" scoped>
